faf00222962de39bf282d7f68768a0aa1df49b76
Furkannkilic <149965842+Furkannkilic@users.noreply.github.com>
2025-01-03 16:43:30 +0300

    Add files via upload

---
+++ player.cpp
@@ -0,0 +1,73 @@
+#include "player.hpp"
+#include "window.hpp"
+
+
+
+
+
+
+player::player(sf::Vector2f place)
+{
+	this->place = place;    
+}
+
+
+
+
+
+
+void player::setYon( Yon newyon)
+{
+	yon = newyon;
+
+}
+
+Yon player::getYon()
+{
+	return yon;
+}
+
+
+
+sf::RectangleShape& player::getShape()
+{
+    return kare;
+}
+
+
+
+
+
+void player::setMoving(bool state) {
+    moving = state;
+}
+
+bool player::isMoving() {
+    return moving;
+}
+
+
+
+
+
+
+
+
+void player::resetPosition(Ball& top)
+{
+    if (top.trf) {
+        // Ball has entered the goal area, reset it to the starting position
+        place = { 924,475 };
+        kare.setPosition(place);
+        moving = false;
+
+    }
+}
+
+
+
+
+
+
+
+
