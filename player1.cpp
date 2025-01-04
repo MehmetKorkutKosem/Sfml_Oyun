855ddf691e3fbff3b12727baed43ae8bc3d1f7be
Furkannkilic <149965842+Furkannkilic@users.noreply.github.com>
2025-01-04 21:10:34 +0300

    Update player1.cpp
    oyuncu1 in hareket güncellemesi

--- player1.cpp
+++ player1.cpp
@@ -6,9 +6,6 @@ void player1::hareket()
 {
     sf::Vector2f a = kare.getSize();
 
-    if (!moving) return;
-
-    std::cout << a.x << "----" << a.y << std::endl;
 
     switch (yon) {
     case Yon::sag:
@@ -72,28 +69,28 @@ void player1::resolveCollision(const sf::RectangleShape& wall)
             place.x -= overlapLeft;  // sola  iter
         }
         else if (overlapRight < overlapLeft && overlapRight < overlapTop && overlapRight < overlapBottom) {
-            place.x += overlapRight;  // saða  iter
+            place.x += overlapRight;  // saÃ°a  iter
         }
         else if (overlapTop < overlapLeft && overlapTop < overlapRight && overlapTop < overlapBottom) {
-            place.y -= overlapTop;  // yukarý iter
+            place.y -= overlapTop;  // yukarÃ½ iter
         }
         else if (overlapBottom < overlapLeft && overlapBottom < overlapRight && overlapBottom < overlapTop) {
-            place.y += overlapBottom;  // aþaðý iter
+            place.y += overlapBottom;  // aÃ¾aÃ°Ã½ iter
         }
     }
 }
 
 bool player1::checkCollisionWithWall(const sf::RectangleShape& wall)
 {
-    sf::FloatRect playerBounds = kare.getGlobalBounds();  // Player'ýn bounding box'ý
-    sf::FloatRect wallBounds = wall.getGlobalBounds();    // Wall'ýn bounding box'ý
+    sf::FloatRect playerBounds = kare.getGlobalBounds();  // Player'Ã½n bounding box'Ã½
+    sf::FloatRect wallBounds = wall.getGlobalBounds();    // Wall'Ã½n bounding box'Ã½
 
     return playerBounds.intersects(wallBounds);
 }
 bool player1::checkCollisionWitCircle(const sf::CircleShape& wall)
 {
-    sf::FloatRect playerBounds = kare.getGlobalBounds();  // Player'ýn bounding box'ý
-    sf::FloatRect wallBounds = wall.getGlobalBounds();    // Wall'ýn bounding box'ý
+    sf::FloatRect playerBounds = kare.getGlobalBounds();  // Player'Ã½n bounding box'Ã½
+    sf::FloatRect wallBounds = wall.getGlobalBounds();    // Wall'Ã½n bounding box'Ã½
 
     return playerBounds.intersects(wallBounds);
 }
@@ -115,13 +112,13 @@ void player1::resolveCollisioncircle(const sf::CircleShape& wall)
             place.x -= 1;  // sola  iter
         }
         else if (overlapRight < overlapLeft && overlapRight < overlapTop && overlapRight < overlapBottom) {
-            place.x += 1;  // saða  iter
+            place.x += 1;  // saÃ°a  iter
         }
         else if (overlapTop < overlapLeft && overlapTop < overlapRight && overlapTop < overlapBottom) {
-            place.y -= 1; // yukarý iter
+            place.y -= 1; // yukarÃ½ iter
         }
         else if (overlapBottom < overlapLeft && overlapBottom < overlapRight && overlapBottom < overlapTop) {
-            place.y += 1;  // aþaðý iter
+            place.y += 1;  // aÃ¾aÃ°Ã½ iter
         }
     }
 }
